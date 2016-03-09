0bf81c986caec700d0a4eb9a4bc48ea8cc187c63
niraj <niraj@niraj-PC>
2016-03-08 22:54:51 -0800

    account Dao Commoited

--- src/main/java/com/basic/nrjbank/dao/AccountDao.java
+++ src/main/java/com/basic/nrjbank/dao/AccountDao.java
@@ -1,18 +1,6 @@
 package com.basic.nrjbank.dao;
 
-public class AccountDao {
-
-	public static void main(String[] args) {
-		System.out.println("niraj");
-	}
-
-	public double getBal(int accno) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public void fundTransfer(int saccno, int daccno, double amt) {
-		// TODO Auto-generated method stub
-		
-	}
+public interface AccountDao {
+	public double getBal(int accno);
+	public void fundTransfer(int saccno, int daccno, double amt); 
 }
